31a115d8a04c7b07eb25946afb6f51b7f161374f
I Made Dwija Abinawa <madeabinawa@gmail.com>
2022-07-05 06:29:47 +0800

    create sigin page

--- src/pages/auth/sign-in/index.js
+++ src/pages/auth/sign-in/index.js
@@ -1,16 +1,52 @@
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { authLogin } from "store/authSlice";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import jwtDecode from "jwt-decode";
+
 // Soft UI Dashboard React components
 import SuiBox from "components/SuiBox";
 import SuiTypography from "components/SuiTypography";
 import SuiInput from "components/SuiInput";
 import SuiButton from "components/SuiButton";
 
+// Api
+import AuthApi from "apis/Auth";
+
 // Authentication layout components
-import CoverLayout from "layouts/authentication/components/CoverLayout";
+import CoverLayout from "../components/CoverLayout";
 
 // Images
-import curved9 from "assets/images/curved-images/curved-6.jpg";
+import curved9 from "../../../assets/images/curved-images/curved-6.jpg";
 
 function SignIn() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const dispatchToken = (data) => dispatch(authLogin({ ...data }));
+
+  const formSubmitHandler = (values, { setSubmitting }) => {
+    AuthApi.signIn(values).then(({ data }) => {
+      const token = data?.data?.token;
+      const decode = jwtDecode(token);
+      const { username, email, name } = decode;
+
+      dispatchToken({ token, name, username, email });
+      navigate("/dashboard", { replace: true });
+    });
+  };
+
+  const formik = useFormik({
+    initialValues: { email: "", password: "" },
+    validationSchema: yup.object().shape({
+      email: yup.string().email("Email tidak valid!").required("Email tidak boleh kosong!"),
+      password: yup.string().required("Password tidak boleh kosong!"),
+    }),
+    onSubmit: formSubmitHandler,
+  });
+
+  const { values, errors, touched, handleChange, isSubmitting, handleSubmit } = formik;
+
   return (
     <CoverLayout
       title="Welcome back"
@@ -24,8 +60,14 @@ function SignIn() {
               Email
             </SuiTypography>
           </SuiBox>
-          {/* // Todo handle input onChange */}
-          <SuiInput type="email" placeholder="Email" />
+          <SuiInput
+            name="email"
+            type="email"
+            placeholder="Email"
+            onChange={handleChange}
+            error={Boolean(errors.email && touched.email)}
+            errorMessage={errors?.email ?? ""}
+          />
         </SuiBox>
         <SuiBox mb={2}>
           <SuiBox mb={1} ml={0.5}>
@@ -33,11 +75,23 @@ function SignIn() {
               Password
             </SuiTypography>
           </SuiBox>
-          {/* // Todo handle input onChange */}
-          <SuiInput type="password" placeholder="Password" />
+          <SuiInput
+            name="password"
+            type="password"
+            placeholder="Password"
+            value={values.password}
+            onChange={handleChange}
+            error={Boolean(errors.password && touched.password)}
+            errorMessage={errors?.password ?? ""}
+          />
         </SuiBox>
         <SuiBox mt={4} mb={1}>
-          <SuiButton variant="gradient" color="info" fullWidth>
+          <SuiButton
+            fullWidth
+            variant="gradient"
+            color="info"
+            onClick={handleSubmit}
+          >
             sign in
           </SuiButton>
         </SuiBox>
