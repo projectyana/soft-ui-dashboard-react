269b8e25a40cff6978a58e7f6a75075ac795e8dd
I Made Dwija Abinawa <madeabinawa@gmail.com>
2022-07-07 22:57:58 +0800

    setup custom Dashboard

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -4,6 +4,7 @@
 
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 import SuiBox from "components/SuiBox";
 import SuiTypography from "components/SuiTypography";
@@ -12,16 +13,17 @@ import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 
 export default function AppDashboard() {
+  const { name, email } = useSelector(state => state.auth);
 
   return (
     <DashboardLayout>
       <DashboardNavbar />
       <SuiBox>
         <SuiTypography variant="h2">
-          Welcome back, <strong>John Doe</strong>
+          Welcome back, <strong>{name}</strong>
         </SuiTypography>
         <SuiTypography variant="subtitle2">
-          Johndoe@email.com
+          {email}
         </SuiTypography>
       </SuiBox>
     </DashboardLayout>
