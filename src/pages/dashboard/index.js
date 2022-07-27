eebc5d663e4576250d2f9112e9490c6c83707c49
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2022-07-27 13:25:50 +0700

    remove SoftAlert #23

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -7,7 +7,6 @@ import { useSelector } from "react-redux";
 
 import SuiBox from "components/SuiBox";
 import SuiTypography from "components/SuiTypography";
-import SoftAlert from "components/SoftAlert";
 
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 
@@ -20,9 +19,8 @@ export default function AppDashboard() {
         <SuiTypography variant="h2">
           Welcome back, <strong>{name}</strong>
         </SuiTypography>
-        <SoftAlert>{email}</SoftAlert>
         <SuiTypography variant="subtitle2">
-          {email}
+          Hola {email} 👋🏻
         </SuiTypography>
       </SuiBox>
     </DashboardLayout>
