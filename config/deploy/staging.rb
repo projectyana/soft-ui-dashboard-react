2b024ea0d62df3a82ab426f7d30f0acb020e171f
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2022-07-27 09:24:15 +0700

    setup capistrano #23

---
+++ config/deploy/staging.rb
@@ -0,0 +1,82 @@
+# server-based syntax
+# ======================
+# Defines a single server with a list of roles and multiple properties.
+# You can define all roles on a single server, or split them:
+
+# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
+# server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
+# server "db.example.com", user: "deploy", roles: %w{db}
+
+set :application,       'rokom-back-office'
+set :stage,             :staging
+set :deploy_env,        :staging
+set :user,              'deploy'
+set :puma_env,          'staging'
+set :full_app_name,     "#{fetch(:applicaton)}-#{fetch(:stage)}"
+set :ssh_options,       { forward_agent: true, auth_methods: %w[publickey] }
+set :deploy_to,         "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :server_name,       "rokom.xyz/admin"
+set :puma_bind,         ["unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"]
+set :puma_state,        "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,          "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log,   "#{release_path}/log/puma.error.log"
+set :puma_error_log,    "#{release_path}/log/puma.access.log"
+
+# http://stackoverflow.com/questions/21036175/how-to-deploy-a-specific-revision-with-capistrano-3
+set :branch, 'staging'
+
+role :app, %w["#{fetch(:user)@ec2-108-136-166-220.ap-southeast-3.compute.amazonaws.com}"]
+role :web, %w["#{fetch(:user)@ec2-108-136-166-220.ap-southeast-3.compute.amazonaws.com}"]
+server 'ec2-108-136-166-220.ap-southeast-3.compute.amazonaws.com', user: fetch(:user), roles: %w[web app]
+#
+# role :app, %w{admin@104.168.96.55}
+# role :web, %w{admin@104.168.96.55}
+# role :db,  %w{admin@104.168.96.55}
+# server '104.168.96.55', user: 'admin', roles: %w{web app}
+
+# role-based syntax
+# ==================
+
+# Defines a role with one or multiple servers. The primary server in each
+# group is considered to be the first unless any hosts have the primary
+# property set. Specify the username and a domain or IP for the server.
+# Don't use `:all`, it's a meta role.
+
+# role :app, %w{deploy@example.com}, my_property: :my_value
+# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
+# role :db,  %w{deploy@example.com}
+
+# Configuration
+# =============
+# You can set any configuration variable like in config/deploy.rb
+# These variables are then only loaded and set in this stage.
+# For available Capistrano configuration variables see the documentation page.
+# http://capistranorb.com/documentation/getting-started/configuration/
+# Feel free to add new variables to customise your setup.
+
+# Custom SSH Options
+# ==================
+# You may pass any option but keep in mind that net/ssh understands a
+# limited set of options, consult the Net::SSH documentation.
+# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
+#
+# Global options
+# --------------
+#  set :ssh_options, {
+#    keys: %w(/home/rlisowski/.ssh/id_rsa),
+#    forward_agent: false,
+#    auth_methods: %w(password)
+#  }
+#
+# The server-based syntax can be used to override options:
+# ------------------------------------
+# server "example.com",
+#   user: "user_name",
+#   roles: %w{web app},
+#   ssh_options: {
+#     user: "user_name", # overrides user setting above
+#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     forward_agent: false,
+#     auth_methods: %w(publickey password)
+#     # password: "please use keys"
+#   }
