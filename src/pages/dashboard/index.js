3838412cf15b1e7391cbcb564ca748a55fa67e6f
I Made Dwija Abinawa <madeabinawa@gmail.com>
2022-07-31 21:12:12 +0800

    adj dashboard

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -24,6 +24,7 @@ import SuiBox from "components/SuiBox";
 import SuiButton from "components/SuiButton";
 import SuiTypography from "components/SuiTypography";
 import BlogCard from "components/Custom/Card/BlogCard";
+import { LoadingState } from "components/Custom/Loading";
 
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import MiniStatisticsCard from "examples/Cards/StatisticsCards/MiniStatisticsCard";
@@ -33,6 +34,7 @@ import ModalBlogDelete from "./components/ModalBlogDelete";
 export default function AppDashboard() {
   const navigate = useNavigate();
   const [dashboard, setDashboard] = useState({});
+  const [fetchStatus, setFetchStatus] = useState({ loading: true });
   const [modalConfig, setModalConfig] = useState({
     type: "delete",
     show: false,
@@ -41,11 +43,14 @@ export default function AppDashboard() {
   const { name, email } = useSelector(state => state.auth);
 
   const fetchData = () => {
+    setFetchStatus({ loading: true });
     DashboardApi.get()
       .then(({ data }) => {
         const mapBlog = data?.data?.recent_blogs?.map(blog => ({ ...blog, link: blog?.thumbnail ? `https://api.rokom.xyz/${blog.thumbnail}` : null }));
         setDashboard({ ...data?.data, recent_blogs: mapBlog ?? [] } ?? {});
-      });
+      })
+      .catch(err => window.alert("Error connect to server!"))
+      .finally(() => setFetchStatus({ loading: false }));
   };
 
   useEffect(() => {
@@ -53,7 +58,6 @@ export default function AppDashboard() {
 
     return () => setDashboard({});
   }, []);
-
 
   return (
     <DashboardLayout>
@@ -65,84 +69,91 @@ export default function AppDashboard() {
           Hola {email} 👋🏻
         </SuiTypography>
 
-        <Grid container mt={2} spacing={3}>
-          <Grid item xs={12} sm={6} xl={3}>
-            <MiniStatisticsCard
-              title={{ text: "Admin" }}
-              count={dashboard?.admin_count}
-              icon={{ color: "info", component: <SupervisorAccount /> }}
-            />
-          </Grid>
+        {fetchStatus.loading
+          ? <LoadingState />
+          : (
+            <>
+              <Grid container mt={2} spacing={3}>
+                <Grid item xs={12} sm={6} xl={3}>
+                  <MiniStatisticsCard
+                    title={{ text: "Admin" }}
+                    count={dashboard?.admin_count}
+                    icon={{ color: "info", component: <SupervisorAccount /> }}
+                  />
+                </Grid>
 
-          <Grid item xs={12} sm={6} xl={3}>
-            <MiniStatisticsCard
-              title={{ text: "Page" }}
-              count={dashboard?.page_count}
-              icon={{ color: "info", component: <Web /> }}
-            />
-          </Grid>
+                <Grid item xs={12} sm={6} xl={3}>
+                  <MiniStatisticsCard
+                    title={{ text: "Page" }}
+                    count={dashboard?.page_count}
+                    icon={{ color: "info", component: <Web /> }}
+                  />
+                </Grid>
 
-          <Grid item xs={12} sm={6} xl={3}>
-            <MiniStatisticsCard
-              title={{ text: "Blog" }}
-              count={dashboard?.blog_count}
-              icon={{ color: "info", component: <Article /> }}
-            />
-          </Grid>
+                <Grid item xs={12} sm={6} xl={3}>
+                  <MiniStatisticsCard
+                    title={{ text: "Blog" }}
+                    count={dashboard?.blog_count}
+                    icon={{ color: "info", component: <Article /> }}
+                  />
+                </Grid>
 
-          <Grid item xs={12} sm={6} xl={3}>
-            <MiniStatisticsCard
-              title={{ text: "Carousel" }}
-              count={dashboard?.carousel_count}
-              icon={{ color: "info", component: <Landscape /> }}
-            />
-          </Grid>
-        </Grid>
+                <Grid item xs={12} sm={6} xl={3}>
+                  <MiniStatisticsCard
+                    title={{ text: "Carousel" }}
+                    count={dashboard?.carousel_count}
+                    icon={{ color: "info", component: <Landscape /> }}
+                  />
+                </Grid>
+              </Grid>
 
-        {/* Recent Blogs */}
-        <SuiTypography mt={4} mb={1} variant="h4">Recent Blogs</SuiTypography>
-        <Grid container spacing={2} >
-          {dashboard?.recent_blogs?.length > 0 && dashboard?.recent_blogs?.map((row, index) => (
-            <Grid
-              style={{ pointer: 'cursor' }}
-              key={row.title} item xs={6} md={4}>
-              <BlogCard
-                style={{ marginRight: 2 }}
-                alt={row.title}
-                image={row.link ?? null}
-                title={row.title}
-                tags={row.tags}
-                slug={row.slug}
-                author={row?.author?.name ?? ""}
-              >
-                <CardActions>
-                  <SuiBox sx={{ width: '100%' }} display="flex" justifyContent="end" alignItems="end" >
-                    <SuiButton
-                      size="small"
-                      variant="text"
-                      color="dark"
-                      onClick={() => navigate("/blog/editor/edit", { state: { ...row } })}
+              {/* Recent Blogs */}
+              <SuiTypography mt={4} mb={1} variant="h4">Recent Blogs</SuiTypography>
+              <Grid container spacing={2} >
+                {dashboard?.recent_blogs?.length > 0 && dashboard?.recent_blogs?.map((row, index) => (
+                  <Grid
+                    style={{ pointer: 'cursor' }}
+                    key={row.title} item xs={6} md={4}>
+                    <BlogCard
+                      style={{ marginRight: 2 }}
+                      alt={row.title}
+                      image={row.link ?? null}
+                      title={row.title}
+                      tags={row.tags}
+                      slug={row.slug}
+                      author={row?.author?.name ?? ""}
                     >
-                      <Icon>edit</Icon> &nbsp;edit
-                    </SuiButton>
+                      <CardActions>
+                        <SuiBox sx={{ width: '100%' }} display="flex" justifyContent="end" alignItems="end" >
+                          <SuiButton
+                            size="small"
+                            variant="text"
+                            color="dark"
+                            onClick={() => navigate("/blog/editor/edit", { state: { ...row } })}
+                          >
+                            <Icon>edit</Icon> &nbsp;edit
+                          </SuiButton>
 
-                    <SuiBox mr={1}>
-                      <SuiButton
-                        size="small"
-                        variant="text"
-                        color="error"
-                        onClick={() => setModalConfig({ show: true, type: "delete", data: row })}
-                      >
-                        <Icon>delete</Icon>&nbsp;delete
-                      </SuiButton>
-                    </SuiBox>
-                  </SuiBox>
-                </CardActions>
-              </BlogCard>
-            </Grid>
-          ))
-          }
-        </Grid>
+                          <SuiBox mr={1}>
+                            <SuiButton
+                              size="small"
+                              variant="text"
+                              color="error"
+                              onClick={() => setModalConfig({ show: true, type: "delete", data: row })}
+                            >
+                              <Icon>delete</Icon>&nbsp;delete
+                            </SuiButton>
+                          </SuiBox>
+                        </SuiBox>
+                      </CardActions>
+                    </BlogCard>
+                  </Grid>
+                ))
+                }
+              </Grid>
+            </>
+          )}
+
       </SuiBox>
 
       {/* Modal Blog Delete */}
