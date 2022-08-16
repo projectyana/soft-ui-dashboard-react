61287510b4dacc6441398e5d5787b7ff0ed559df
I Made Dwija Abinawa <madeabinawa@gmail.com>
2022-08-16 15:27:21 +0800

    show all file upload categories, close #44

--- src/pages/gallery/components/files_view.js
+++ src/pages/gallery/components/files_view.js
@@ -1,5 +1,8 @@
-import React, { useEffect, useState } from 'react';
+/* eslint-disable */
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
 import {
+  Chip,
   Table,
   TableBody,
   TableCell,
@@ -8,7 +11,7 @@ import {
   Paper,
   Icon,
   Tooltip,
-  ButtonGroup
+  ButtonGroup,
 } from "@mui/material";
 import FileSaver from "file-saver";
 
@@ -30,19 +33,36 @@ import ModalDelete from "./file_upload/ModalDelete";
 const FilesView = ({ modalCreate, setModalCreate }) => {
   const [loading, setLoading] = useState({ fetch: true });
   const [data, setData] = useState([]);
+  const [dropdown, setDropdown] = useState({
+    categories: [],
+  });
+  const [selectedCat, setSelectedCat] = useState("");
   const [modalDelete, setModalDelete] = useState({
     show: false,
-    data: null
+    data: null,
   });
   const [modalConfigCategory, setModalConfigCategory] = useState({ show: false });
 
-  const fetchData = () => {
-    setLoading({ fetch: true });
-    FileUploadApi.get()
-      .then(res => {
-        const files = res.data.data.map(item => ({ ...item, full_path: `https://api.rokom.xyz/${item.path}` }));
-        setData(files ?? []);
-      })
+  const fetchData = (fetch = true) => {
+    setLoading({ fetch });
+
+    Axios.all([FileUploadApi.get(), FileUploadApi.getCategories()])
+      .then(
+        Axios.spread((res, resCat) => {
+          const files = res.data.data.map((item) => ({
+            ...item,
+            full_path: `https://api.rokom.xyz/${item.path}`,
+          }));
+          const mapDropdown =
+            resCat?.data?.data
+              ?.map(({ sub_categories }) => sub_categories ?? [])
+              ?.flat()
+              ?.map((item) => ({ ...item, value: item.id, label: item.name })) ?? [];
+
+          setDropdown({ categories: [{ value: "", label: "All" }].concat(mapDropdown) });
+          setData(files ?? []);
+        })
+      )
       .finally(() => setLoading({ fetch: false }));
   };
 
@@ -51,18 +71,22 @@ const FilesView = ({ modalCreate, setModalCreate }) => {
     FileSaver.saveAs(url, fileName);
   };
 
+  const filterData = selectedCat ? data.filter((val) => val.doc_category_id === selectedCat) : data;
+
   useEffect(() => {
     fetchData();
-    return () => { setData([]); };
-
+    return () => {
+      setData([]);
+    };
   }, []);
 
   if (loading.fetch) {
     return <LoadingState />;
   }
+
   return (
     <>
-      <SuiBox mb={1} display="flex" justifyContent="start" alignItems="center" >
+      <SuiBox mb={1} display="flex" justifyContent="start" alignItems="center">
         <SuiTypography variant="h2">Categories</SuiTypography>
         <ButtonGroup>
           <Tooltip title="Create new File Category">
@@ -72,7 +96,8 @@ const FilesView = ({ modalCreate, setModalCreate }) => {
               iconOnly
               size="small"
               color="info"
-              onClick={() => setModalConfigCategory({ show: true, type: 'create' })} >
+              onClick={() => setModalConfigCategory({ show: true, type: "create" })}
+            >
               <Icon>add</Icon>
             </SuiButton>
           </Tooltip>
@@ -84,13 +109,26 @@ const FilesView = ({ modalCreate, setModalCreate }) => {
               iconOnly
               size="small"
               color="success"
-              onClick={() => setModalConfigCategory({ show: true, type: 'edit' })} >
+              onClick={() => setModalConfigCategory({ show: true, type: "edit" })}
+            >
               <Icon>edit</Icon>
             </SuiButton>
           </Tooltip>
-
         </ButtonGroup>
-      </SuiBox >
+      </SuiBox>
+
+      <SuiBox pb={2} display="flex" flex="wrap" justifyContent="start" alignItems="center">
+        {dropdown?.categories?.length > 0 &&
+          dropdown?.categories?.map((row) => (
+            <Chip
+              sx={{ margin: 0.5 }}
+              label={row.label}
+              onClick={() => setSelectedCat(row.value)}
+              variant="outlined"
+              color={row.value === selectedCat ? "info" : "secondary"}
+            />
+          ))}
+      </SuiBox>
       <TableContainer component={Paper}>
         <Table aria-label="simple table">
           <TableRow>
@@ -102,37 +140,39 @@ const FilesView = ({ modalCreate, setModalCreate }) => {
             </TableCell>
           </TableRow>
           <TableBody>
-            {data?.length > 0 ? data.map((row) => (
-              <TableRow key={row.name} >
-                <TableCell component="th" scope="row">
-                  <SuiTypography variant="caption">{row.name}</SuiTypography>
-                </TableCell>
-                <TableCell>
-                  <SuiBox display="flex" alignItems="center">
-                    <SuiBox mr={1}>
-                      <SuiButton
-                        size="small"
-                        variant="text"
-                        color="dark"
-                        onClick={() => handleDownload(row.full_path)}
-                      >
-                        <Icon>download</Icon>&nbsp;download
-                      </SuiButton>
+            {filterData?.length > 0 ? (
+              filterData.map((row) => (
+                <TableRow key={row.name}>
+                  <TableCell component="th" scope="row">
+                    <SuiTypography variant="caption">{row.name}</SuiTypography>
+                  </TableCell>
+                  <TableCell>
+                    <SuiBox display="flex" alignItems="center">
+                      <SuiBox mr={1}>
+                        <SuiButton
+                          size="small"
+                          variant="text"
+                          color="dark"
+                          onClick={() => handleDownload(row.full_path)}
+                        >
+                          <Icon>download</Icon>&nbsp;download
+                        </SuiButton>
+                      </SuiBox>
+                      <SuiBox mr={1}>
+                        <SuiButton
+                          size="small"
+                          variant="text"
+                          color="error"
+                          onClick={() => setModalDelete({ show: true, type: "delete", data: row })}
+                        >
+                          <Icon>delete</Icon>&nbsp;delete
+                        </SuiButton>
+                      </SuiBox>
                     </SuiBox>
-                    <SuiBox mr={1}>
-                      <SuiButton
-                        size="small"
-                        variant="text"
-                        color="error"
-                        onClick={() => setModalDelete({ show: true, type: "delete", data: row })}
-                      >
-                        <Icon>delete</Icon>&nbsp;delete
-                      </SuiButton>
-                    </SuiBox>
-                  </SuiBox>
-                </TableCell>
-              </TableRow>
-            )) : (
+                  </TableCell>
+                </TableRow>
+              ))
+            ) : (
               <TableRow>
                 <TableCell align="center" colSpan={4}>
                   <SuiTypography variant="h6">No files found</SuiTypography>
@@ -144,24 +184,39 @@ const FilesView = ({ modalCreate, setModalCreate }) => {
       </TableContainer>
 
       {/* Modal  Create */}
-      {modalCreate.show && modalCreate.type === "file" && <ModalCreate fetchData={fetchData} modalConfig={modalCreate} setModalConfig={setModalCreate} />}
+      {modalCreate.show && modalCreate.type === "file" && (
+        <ModalCreate
+          fetchData={fetchData}
+          modalConfig={modalCreate}
+          setModalConfig={setModalCreate}
+        />
+      )}
 
       {/* Modal Delete */}
-      {modalDelete.show && <ModalDelete fetchData={fetchData} modalConfig={modalDelete} setModalConfig={setModalDelete} />}
+      {modalDelete.show && (
+        <ModalDelete
+          fetchData={fetchData}
+          modalConfig={modalDelete}
+          setModalConfig={setModalDelete}
+        />
+      )}
 
       {/* Modal Create Category */}
-      {modalConfigCategory.show && modalConfigCategory.type === "create" &&
+      {modalConfigCategory.show && modalConfigCategory.type === "create" && (
         <ModalCreateCategory
           modalConfigCategory={modalConfigCategory}
           setModalConfig={setModalConfigCategory}
-        />}
+        />
+      )}
 
       {/* Modal Edit Category */}
-      {modalConfigCategory.show && modalConfigCategory.type === "edit" &&
+      {modalConfigCategory.show && modalConfigCategory.type === "edit" && (
         <ModalEditCategory
+          fetchData={fetchData}
           modalConfigCategory={modalConfigCategory}
           setModalConfig={setModalConfigCategory}
-        />}
+        />
+      )}
     </>
   );
 };
